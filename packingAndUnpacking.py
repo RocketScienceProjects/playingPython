2827e2c051494cb546707fb14988202e77a6cc75
okram999 <nirish.okram@gmail.com>
2017-05-11 14:15:30 -0500

    more on packing and unpack

--- packingAndUnpacking.py
+++ packingAndUnpacking.py
@@ -11,9 +11,27 @@ print(str)
 print(*str)
 #gives a b c d e f
 ################################
-
+#packing- used in undefined number of parameters
 def add(*numbers):
     total = 0
     for number in numbers:
         total = total + number
     return total
+
+#################################
+#keyword args (kwargs)
+
+my_dict = {"Name": "Arie", "Age": 1, "Sex": "male"}
+
+def about(Name,Age,Sex):
+    return("{} is {} month old and is a {}".format(Name,Age,Sex))
+
+about(**my_dict)  ##note the ** for keyword arguments
+
+##################################
+
+def something(**kwargs):
+    for key, value in kwargs.items():
+        print("{}:{}".format(key,value))
+
+something(male = "Ali", female = "Zii")
